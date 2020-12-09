49da819c9c51b3969f900efec88bd1fa2cccfe72
Cogee <cogansam@gmail.com>
2020-12-09 20:37:06 +0000

    Array created, random logic implemented

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) {
-        
+        //vars
+        String[] gameWords={"january","february","march"};
+        int lives=6;
+        int randomPick=(int)(Math.random()*gameWords.length);
+        String playWord=gameWords[randomPick];
+        //game logic
+        for(int i=0;i<gameWords.length;i++){
+            System.out.println(gameWords[i]);
+        }
+        System.out.println("Play: "+playWord);
     }
 }
